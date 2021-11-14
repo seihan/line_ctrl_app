9bb8f2c036b8def92bf59fff370739090d745d99
Hannes Seidel <greenredact@gmail.com>
2021-11-14 11:16:17 +0100

    added reverse value to spin opposite motor back

--- lib/controller/line_controller.dart
+++ lib/controller/line_controller.dart
@@ -13,13 +13,14 @@ class LineController {
   bool _send = false;
   bool _connected = false;
   bool _left = false;
+  int _bothValue = 0;
 
   void init() {
     _initBluetoothController();
     _initSensorController();
     _connectionState =
         _bluetoothController.connected.listen(_handleConnectionState);
-    _timer = Timer.periodic(const Duration(milliseconds: 100), _timerCallback);
+    _timer = Timer.periodic(const Duration(milliseconds: 200), _timerCallback);
   }
 
   void _timerCallback(Timer timer) {
@@ -47,23 +48,32 @@ class LineController {
     if (_connected && _send) {
       if (vector2.x < 1) {
         if (vector2.y < 0) {
-          _bluetoothController.write(
-              type: ControllerType.left, value: vector2.y.toInt());
+          if (_left) {
+            _bluetoothController.write(
+                type: ControllerType.left, value: vector2.y.toInt());
+          } else {
+            _bluetoothController.write(
+                type: ControllerType.right, value: vector2.y.toInt() * -1);
+          }
         } else {
-          _bluetoothController.write(
-              type: ControllerType.right, value: vector2.y.toInt());
+          if (_left) {
+            _bluetoothController.write(
+                type: ControllerType.left, value: vector2.y.toInt() * -1);
+          } else {
+            _bluetoothController.write(
+                type: ControllerType.right, value: vector2.y.toInt());
+          }
         }
       } else {
         if (_left) {
+          _bothValue = vector2.x.toInt();
           _bluetoothController.write(
-              type: ControllerType.left, value: vector2.x.toInt());
-        }
-        if (!_left) {
+              type: ControllerType.left, value: _bothValue);
+        } else {
           _bluetoothController.write(
-              type: ControllerType.right, value: vector2.x.toInt());
+              type: ControllerType.right, value: _bothValue);
         }
       }
-
       _send = false;
     }
   }
