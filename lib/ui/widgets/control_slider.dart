6a0c9513d2f477a67881b20b707bbad100bb1ef9
Hannes Seidel <greenredact@gmail.com>
2023-05-12 17:40:58 +0200

    replaced slider with xslider package

--- lib/ui/widgets/control_slider.dart
+++ lib/ui/widgets/control_slider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_xlider/flutter_xlider.dart';
 
 class ControlSlider extends StatelessWidget {
   final double value;
@@ -7,14 +8,14 @@ class ControlSlider extends StatelessWidget {
   final bool active;
   final String title;
 
-  const ControlSlider(
-      {Key? key,
-      required this.value,
-      required this.onChanged,
-      this.onPressed,
-      this.active = false,
-      this.title = 'no title'})
-      : super(key: key);
+  const ControlSlider({
+    Key? key,
+    required this.value,
+    required this.onChanged,
+    this.onPressed,
+    this.active = false,
+    this.title = 'no title',
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,33 +24,68 @@ class ControlSlider extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.only(top: 25),
           child: Text(
-            value.toString(),
+            value.round().toString(),
             textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 17, color: Colors.white),
           ),
         ),
         Expanded(
-          child: RotatedBox(
-            quarterTurns: 3,
-            child: Slider(
-              min: -255,
-              max: 255,
-              value: value,
-              secondaryTrackValue: 0,
-              inactiveColor: Colors.white24,
-              secondaryActiveColor: Colors.red,
-              activeColor: value == 0
-                  ? Colors.grey
-                  : value < 0
-                      ? Colors.white24
-                      : Colors.green,
-              thumbColor: value == 0
-                  ? Colors.grey
-                  : value < 0
-                      ? Colors.red
-                      : Colors.green,
-              label: value.round().toString(),
-              onChanged: onChanged,
+          child: FlutterSlider(
+            values: [value],
+            max: 255,
+            min: -255,
+            rtl: true,
+            step: const FlutterSliderStep(step: 1),
+            centeredOrigin: true,
+            trackBar: FlutterSliderTrackBar(
+              activeTrackBarHeight: 30,
+              centralWidget: const Icon(
+                Icons.remove,
+                color: Colors.white,
+              ),
+              activeTrackBar:
+                  BoxDecoration(color: value < 0 ? Colors.red : Colors.green),
+              inactiveTrackBar: BoxDecoration(
+                color: active ? Colors.white : Colors.grey,
+              ),
             ),
+            axis: Axis.vertical,
+            handlerWidth: 60,
+            handler: FlutterSliderHandler(
+              decoration: const BoxDecoration(),
+              child: Container(
+                height: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(6),
+                  color: active ? Colors.white : Colors.grey,
+                ),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 13, horizontal: 2),
+                child: Container(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            tooltip: FlutterSliderTooltip(
+              disabled: true,
+              textStyle: const TextStyle(fontSize: 17, color: Colors.white),
+              boxStyle: FlutterSliderTooltipBox(
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.7),
+                ),
+              ),
+              format: (String value) {
+                return value.split('.')[0];
+              },
+            ),
+            onDragging: (handlerIndex, lowerValue, upperValue) {
+              if (value != lowerValue) {
+                debugPrint(
+                  'handlerIndex: $handlerIndex, lowerValue: $lowerValue, upperValue: $upperValue, value: $value',
+                );
+                onChanged(lowerValue);
+              }
+            },
           ),
         ),
         Padding(
