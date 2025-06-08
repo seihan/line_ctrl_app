09aa78333f37b414f6c37f8600cb3741404ea107
Hannes Seidel <greenredact@gmail.com>
2025-06-08 18:00:13 +0200

    upgraded gradle to 8.5.1; set package name to org.linctrl.app

---
+++ android/app/src/main/kotlin/org/linectrl/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.linectrl.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/org/linectrl/line_ctrl_app/MainActivity.kt
+++ android/app/src/main/kotlin/org/linectrl/line_ctrl_app/MainActivity.kt
@@ -2,5 +2,4 @@ package org.linectrl.line_ctrl_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
